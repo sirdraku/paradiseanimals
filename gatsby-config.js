66c1c7807a5e5c0703b6045e320094177f93fb48
sirdraku <83011440+sirdraku@users.noreply.github.com>
2021-06-10 13:09:19 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
           },
         },
         // url: process.env.WPGRAPHQL_URL,
-        url: `https://web.amanuntul.ro/graphql`,
+        url: `https://www.paradiseanimals.com/graphql`,
         verbose: true,
         develop: {
           hardCacheMediaFiles: false,
